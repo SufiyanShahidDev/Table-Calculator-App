a7ba440b1b88153b52f6be417a108aab8af42a01
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2026-01-01 16:49:59 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// let showResult = ""
+
+// function result() {
+//     let table = +(document.getElementById("table").value)
+//     let range = +(document.getElementById("range").value)
+
+//     // console.log(table);
+//     // console.log(range);
+
+//     for (i = 1; i <= range; i++) {
+//         showResult += `${table} x ${i} = ${table * i}<br>`
+//         document.getElementById("result").innerHTML = showResult
+//     }
+//     document.getElementById("table").value = ""
+//     document.getElementById("range").value = ""
+// }
+
+
+// function reload() {
+//     window.location.reload();
+// }
