b69e151a8a7f5d67f7f803984499dc55bef43aec
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2026-01-01 16:50:48 +0500

    Initial commit

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// let showResult = ""
+let showResult = ""
 
-// function result() {
-//     let table = +(document.getElementById("table").value)
-//     let range = +(document.getElementById("range").value)
+function result() {
+    let table = +(document.getElementById("table").value)
+    let range = +(document.getElementById("range").value)
 
-//     // console.log(table);
-//     // console.log(range);
+    // console.log(table);
+    // console.log(range);
 
-//     for (i = 1; i <= range; i++) {
-//         showResult += `${table} x ${i} = ${table * i}<br>`
-//         document.getElementById("result").innerHTML = showResult
-//     }
-//     document.getElementById("table").value = ""
-//     document.getElementById("range").value = ""
-// }
+    for (i = 1; i <= range; i++) {
+        showResult += `${table} x ${i} = ${table * i}<br>`
+        document.getElementById("result").innerHTML = showResult
+    }
+    document.getElementById("table").value = ""
+    document.getElementById("range").value = ""
+}
 
 
-// function reload() {
-//     window.location.reload();
-// }
+function reload() {
+    window.location.reload();
+}
